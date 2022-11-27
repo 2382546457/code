e14edf2a96824b616e887afd87b83200b5d40239
2382546457 <2382546457@qq.com>
2022-11-27 16:37:11 +0800

    测试提交

---
+++ demo01/src/main/java/com/xiaohe/demo01/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.xiaohe.demo01.controller;
+
+
+import java.util.Scanner;
+
+/**
+ * @author : 小何
+ * @Description :
+ * @date : 2022-11-27 16:35
+ */
+
+public class UserController {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println();
+    }
+
+}
