bae7d8999ab283abe82caf323acfedef738dca14
2382546457 <2382546457@qq.com>
2022-11-27 17:15:58 +0800

    测试提交

---
+++ demo02/src/main/java/com/xiaohe/demo02/mapper/EmployeeMapper.java
@@ -0,0 +1,12 @@
+package com.xiaohe.demo02.mapper;
+
+/**
+ * @author : 小何
+ * @Description :
+ * @date : 2022-11-27 17:15
+ */
+public class EmployeeMapper {
+    public String process() {
+        return "你好";
+    }
+}
